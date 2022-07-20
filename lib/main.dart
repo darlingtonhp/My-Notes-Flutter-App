9943667f2bc3fe7980c418745d1205408318c336
Darlington Takunda Hapanamambo <43202203+Darl997@users.noreply.github.com>
2022-07-20 21:46:13 +0200

    Step-26

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:realpj/helpers/loading/loading_screen.dart';
 import 'package:realpj/services/auth/bloc/auth_bloc.dart';
 import 'package:realpj/services/auth/bloc/auth_event.dart';
 import 'package:realpj/services/auth/bloc/auth_state.dart';
@@ -34,21 +35,35 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     context.read<AuthBloc>().add(const AuthEventInitialize());
-    return BlocBuilder<AuthBloc, AuthState>(
-      builder: (context, state) {
-        if (state is AuthStateLoggedIn) {
-          return const NotesView();
-        } else if (state is AuthStateNeedsVerification) {
-          return const VerifyEmailView();
-        } else if (state is AuthStateLoggedOut) {
-          return const LoginView();
-        } else if (state is AuthStateRegistering) {
-          return const RegisterView();
-        } else {
-          return const Scaffold(
-            body: CircularProgressIndicator(),
+    return BlocConsumer<AuthBloc, AuthState>(
+      listener: (context, state) {
+        if (state.isLoading) {
+          LoadingScreen().show(
+            context: context,
+            text: state.loadingText ?? 'Please wait a moment',
           );
+        } else {
+          LoadingScreen().hide();
         }
+      },
+      builder: (context, state) {
+        return BlocBuilder<AuthBloc, AuthState>(
+          builder: (context, state) {
+            if (state is AuthStateLoggedIn) {
+              return const NotesView();
+            } else if (state is AuthStateNeedsVerification) {
+              return const VerifyEmailView();
+            } else if (state is AuthStateLoggedOut) {
+              return const LoginView();
+            } else if (state is AuthStateRegistering) {
+              return const RegisterView();
+            } else {
+              return const Scaffold(
+                body: CircularProgressIndicator(),
+              );
+            }
+          },
+        );
       },
     );
   }
